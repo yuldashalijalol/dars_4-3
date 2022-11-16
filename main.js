c0a3130399bb742d46029c03cd41177faba826fe
yuldashalijalol <115680675+yuldashalijalol@users.noreply.github.com>
2022-11-16 22:30:18 +0500

    dars_4-3

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+let a = prompt("Harif va sonlarni qo'shib yozing va javobini ko'ring");
+
+let i = -1;
+
+ 
+while(i < a.length -1){
+
+  i++
+  if(a[i] = Number(a[i])) {continue}
+
+  document.write(String(a[i]));
+
+  console.log(String(a[i]));
+}
